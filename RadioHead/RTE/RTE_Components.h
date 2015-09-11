7a53dddb83185f1864b0c0ce3072db844d8ddadd
x893 <x893.00@gmail.com>
2015-09-11 17:26:32 +0300

    Add Radiohead STM32L1xx

---
+++ RadioHead/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RF95_reliable_datagram' 
+ * Target:  'RF95_SERVER' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
